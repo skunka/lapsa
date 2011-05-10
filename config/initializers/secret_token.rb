9abb1e0f0e1e39a3de7887890cf6735f4f4d1036
skunka <andrius@andajus.com>
2011-05-10 13:58:50 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lapsa::Application.config.secret_token = '681ab04e678da77330026093e3cd42907442ae934b31522bfe4a20ef6300e8ed23bcbf81196e281a2f68533a7b276ffd7940ecfb1546791b8ea05d86da504d46'
